caacf612692f480e66ac1c2df70c3d3fa2199733
Bob Hammell <rhammell@gmail.com>
2021-09-09 20:10:42 -0400

    Updated tflite file; Refactored notification light code

--- ei_image_classification.py
+++ ei_image_classification.py
@@ -1,15 +1,39 @@
 # Edge Impulse - OpenMV Image Classification Example
 
-import sensor, image, time, os, tf
+import sensor, image, time, os, tf, pyb
+from pyb import Pin
 
 sensor.reset()                         # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.GRAYSCALE)    # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_pixformat(sensor.GRAYSCALE) # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.QVGA)      # Set frame size to QVGA (320x240)
 sensor.set_windowing((240, 240))       # Set 240x240 window.
 sensor.skip_frames(time=2000)          # Let the camera adjust.
 
 net = "trained.tflite"
 labels = [line.rstrip('\n') for line in open("labels.txt")]
+
+# Define output pins for RGB LED
+pin_r = Pin("P7", Pin.OUT_PP)
+pin_g = Pin("P8", Pin.OUT_PP)
+pin_b = Pin("P9", Pin.OUT_PP)
+
+# Function to set RGB LED color based on input label
+def color_by_label(label):
+    if label == 'flat':
+        # Red
+        pin_r.high()
+        pin_g.low()
+        pin_b.low()
+    elif label == 'full':
+        # Green
+        pin_r.low()
+        pin_g.high()
+        pin_b.low()
+    elif label == 'no-tire':
+        # Yellow
+        pin_r.high()
+        pin_g.high()
+        pin_b.low()
 
 clock = time.clock()
 while(True):
@@ -24,7 +48,12 @@ while(True):
         # This combines the labels and confidence values into a list of tuples
         predictions_list = list(zip(labels, obj.output()))
 
+        # Update RGB LED color based on the highest predicted class
+        max_prediction = max(predictions_list, key=lambda item:item[1])
+        color_by_label(max_prediction[0])
+
         for i in range(len(predictions_list)):
-            print("%s = %f" % (predictions_list[i][0], predictions_list[i][1]))
+            #print("%s = %f" % (predictions_list[i][0], predictions_list[i][1]))
+            img.draw_string(5, 10*i+5,"%s = %f" % (predictions_list[i][0].upper(), predictions_list[i][1]))
 
     print(clock.fps(), "fps")
